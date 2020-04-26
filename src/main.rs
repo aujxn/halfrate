d4d0023b36aea6561dac1dfe2522089759ade762
aujxn <ajn6@pdx.edu>
2020-04-26 15:13:41 -0700

    cli parser

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
-use riff_wave::WaveWriter;
+use hound::WavReader;
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    // The wav file to resample
+    #[structopt(short, long)]
+    file: String,
+
+    // The target sample rate as a fraction of the old rate.
+    // Default value is 0.5.
+    #[structopt(short, long, default_value = "0.5")]
+    rate: f64,
+}
 
 fn main() {
-    println!("Hello, world!");
+    //let wav = WavReader::open(file).unwrap();
 }
