7690934c3dcaaf01bd7c5b04bd09a6c00b7b8d95
aujxn <ajn6@pdx.edu>
2020-04-26 19:01:07 -0700

    cleaned up

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use hound::{SampleFormat, WavReader, WavSamples, WavSpec, WavWriter};
+use hound::{SampleFormat, WavReader, WavSpec, WavWriter};
 use std::fs::File;
 use std::io::BufWriter;
 use structopt::StructOpt;
@@ -8,21 +8,10 @@ struct Opt {
     // The wav file to resample
     #[structopt(short, long, default_value = "gc.wav sine.wav synth.wav")]
     files: String,
-
-    // The target sample rate as a fraction of the old rate.
-    // Default value is 0.5.
-    // Must be 0.0 < x < 1.0
-    #[structopt(short, long, default_value = "0.5")]
-    rate: f64,
 }
 
 fn main() {
     let opt = Opt::from_args();
-
-    if opt.rate <= 0.0 || opt.rate >= 1.0 {
-        println! {"rate must be 0.0 < x < 1.0"};
-        return;
-    }
 
     let files: Vec<String> = opt
         .files
@@ -34,53 +23,38 @@ fn main() {
     for in_file in files {
         let mut wav = WavReader::open(&in_file).unwrap();
         let spec = wav.spec();
-        let target_rate = spec.sample_rate as f64 * opt.rate;
 
-        println!(
-            "File: {}\n{:?}\nTarget Rate: {}\n",
-            in_file, spec, target_rate
-        );
+        println!("File: {}\n{:?}\n", in_file, spec);
 
-        let samples: WavSamples<_, i16> = wav.samples();
-
-        let filtered = convolve(samples.map(|x| x.unwrap() as f64).collect());
-
-        let f64_to_i16_safe = |x: f64| -> i16 {
-            let max = std::i16::MAX as f64;
-            if x > max {
-                std::i16::MAX
-            } else if x < -1.0 * max {
-                -1 * std::i16::MAX
-            } else {
-                x as i16
-            }
-        };
-
-        let decimated = filtered
-            .into_iter()
-            .step_by(2)
-            .map(|x| f64_to_i16_safe(x))
-            .collect::<Vec<i16>>();
-
-        let out_file = File::create("r".to_string() + &in_file).unwrap();
-        let writer = BufWriter::new(out_file);
-
-        let spec = WavSpec {
-            channels: 1,
-            sample_rate: target_rate as u32,
-            bits_per_sample: 16,
-            sample_format: SampleFormat::Int,
-        };
-
-        let mut writer = WavWriter::new(writer, spec).unwrap();
-        for sample in decimated {
-            writer.write_sample(sample);
-        }
-        writer.finalize();
+        let samples = wav
+            .samples()
+            .map(|x: Result<i16, _>| x.unwrap() as f64)
+            .collect();
+        let decimated = convolve_and_decimate(samples);
+        write_wav(decimated, in_file);
     }
 }
 
-fn convolve(input: Vec<f64>) -> Vec<f64> {
+fn write_wav(samples: Vec<i16>, in_file: String) {
+    let out_file = File::create("r".to_string() + &in_file).unwrap();
+    let writer = BufWriter::new(out_file);
+
+    let spec = WavSpec {
+        channels: 1,
+        sample_rate: 24000,
+        bits_per_sample: 16,
+        sample_format: SampleFormat::Int,
+    };
+
+    let mut writer = WavWriter::new(writer, spec).unwrap();
+    for sample in samples {
+        writer.write_sample(sample).unwrap();
+    }
+    writer.finalize().unwrap();
+}
+
+fn convolve_and_decimate(input: Vec<f64>) -> Vec<i16> {
+    // low half pass FIR coefficients
     let coef: [f64; 91] = [
         0.0007402732643054117,
         -0.0007116178196449282,
@@ -175,14 +149,34 @@ fn convolve(input: Vec<f64>) -> Vec<f64> {
         0.0007402732643054117,
     ];
 
-    // grab the first 90 samples unchanged
-    let mut output = Vec::new();
-    output.extend_from_slice(&input[0..90]);
+    // grab the first 45 samples unchanged
+    let mut output: Vec<i16> = input
+        .iter()
+        .take(90)
+        .step_by(2)
+        .map(|&x| x as i16)
+        .collect();
 
+    // vector mult
     let dot = |x: &[f64], y: &[f64]| x.iter().zip(y.iter()).fold(0.0, |acc, (x, y)| acc + x * y);
 
-    for x in 91..input.len() {
-        output.push(dot(&coef, &input[x - 91..x]));
+    // convert f64 to i16 without overflow
+    let f64_to_i16_safe = |x: f64| -> i16 {
+        let max = std::i16::MAX as f64;
+        if x > max {
+            std::i16::MAX
+        } else if x < -1.0 * max {
+            -1 * std::i16::MAX
+        } else {
+            x as i16
+        }
+    };
+
+    // convolve coefficients over input and skip every other sample to half the rate
+    for x in (91..input.len()).step_by(2) {
+        let product = dot(&coef, &input[x - 91..x]);
+        output.push(f64_to_i16_safe(product));
     }
+
     output
 }
